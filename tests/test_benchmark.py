1536d2c3a30a5dc6b57d72ea539d423ed73ac591
marcus-k <81660172+marcus-k@users.noreply.github.com>
2026-01-27 14:26:51 -0700

    Add MNA test

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -49,8 +49,16 @@ def test_wire_density(NWN: str, request: pytest.FixtureRequest) -> None:
 def test_NWN_units(NWN: str, request: pytest.FixtureRequest) -> None:
     NWN: NanowireNetwork = request.getfixturevalue(NWN)
     units = {
-        "v0": 1.0, "Ron": 20.0, "l0": 1.0, "D0": 60.0, "w0": 10.0,
-        "rho0": 22.63676, "mu0": 1e-2, "Roff_Ron": 160, "i0": 0.05, "t0": 10000
+        "v0": 1.0,
+        "Ron": 20.0,
+        "l0": 1.0,
+        "D0": 60.0,
+        "w0": 10.0,
+        "rho0": 22.63676,
+        "mu0": 1e-2,
+        "Roff_Ron": 160,
+        "i0": 0.05,
+        "t0": 10000,
     }
     for k1, k2 in zip(units.keys(), NWN.units.keys()):
         assert abs(units[k1] - NWN.units[k2]) < 1e-8
@@ -90,6 +98,44 @@ def test_JDA_resistance(NWN_benchmark_JDA: NanowireNetwork) -> None:
     assert abs(R - R_JDA) < 1e-8
 
 
+def test_JDA_MNA(NWN_benchmark_JDA: NanowireNetwork) -> None:
+    # Get benchmark network
+    NWN = NWN_benchmark_JDA
+    assert NWN.graph["type"] == "JDA"
+
+    # Benchmark values
+    Gj = 1
+
+    # Nodal admittance matrix
+    G = Gj * np.array(
+        [
+            [1, 0, 0, 0, -1, 0],
+            [0, 2, -1, 0, 0, -1],
+            [0, -1, 3, 0, -1, -1],
+            [0, 0, 0, 0, 0 ,0],
+            [-1, 0, -1, 0, 2, 0],
+            [0, -1, -1, 0, 0, 2]
+        ]
+    )
+
+    # Compare G matrix
+    mnns_G = mnns.create_matrix(
+        NWN, "conductance", [(0,)], [], False
+    ).toarray()
+    assert np.sum(np.abs(G - mnns_G)) < 1e-12
+
+    # Ground a node
+    G[1, :] = 0
+    G[:, 1] = 0
+    G[1, 1] = 1
+
+    # Compare G matrix with grounded node
+    mnns_G = mnns.create_matrix(
+        NWN, "conductance", [(0,)], [(1,)], False
+    ).toarray()
+    assert np.sum(np.abs(G - mnns_G)) < 1e-12
+
+
 def test_MNR_resistance(NWN_benchmark_MNR: NanowireNetwork) -> None:
     # Get benchmark network
     NWN = NWN_benchmark_MNR
@@ -103,12 +149,13 @@ def test_MNR_resistance(NWN_benchmark_MNR: NanowireNetwork) -> None:
     R *= units["Ron"]
 
     # Check for the correct MNR resistance
-    const = units["rho0"] / (np.pi/4 * units["D0"]**2) * 1e3
+    const = units["rho0"] / (np.pi / 4 * units["D0"] ** 2) * 1e3
     Rin1 = const * 1.2
     Rin2 = const * np.hypot(0.3, 0.3)
     Rin3 = const * np.hypot(1.5, 1.5)
     Rin4 = const * 1.5
 
-    R_MNR = 20 + Rin1 + 20 + Rin2 + \
-        1 / (1 / (Rin3 + 20) + 1 / (20 + Rin4 + 20))    # ~74.618
+    R_MNR = (
+        20 + Rin1 + 20 + Rin2 + 1 / (1 / (Rin3 + 20) + 1 / (20 + Rin4 + 20))
+    )  # ~74.618
     assert abs(R - R_MNR) < 1e-8
